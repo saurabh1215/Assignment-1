1fa88a9399fe23576e87f15064bea5b7e026ad2b
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-12-01 19:18:28 +0530

    +

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <div className="flex flex-col overflow-x-auto">
-      <div className="bg-[#FEF9F6] bg-no-repeat bg-cover bg-center">
-        <Navbar />
+      <div className="bg-[#FEF9F6] bg-no-repeat bg-cover bg-center ">
+        <div className="border-b-[1px]">
+          <Navbar />
+        </div>
+
         <Hero />
       </div>
     </div>
